43cfca4fe57789b92268dfa1fece6fb4a6da0fcf
picerusonix <62857510+picerusonix@users.noreply.github.com>
2020-03-30 21:34:07 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,14 @@
 #include "gra.h"
-#include <iostream>
+#define SDL_MAIN_HANDLED
 gra* Gra = nullptr;
 
-
-
-int main(int argc, const char* argv[])
+int main(int argc, char** argv)
 {
 	Gra = new gra();
-	Gra->init("silnik", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, false);
+	Gra->init("silnik", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1920, 600, false);
 		
-	while(Gra->running()){
+	while(Gra->running())
+	{
 		Gra->handleevent();
 		Gra->update();
 		Gra->render();
