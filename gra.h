b1c7234cf466de302c3d18b9ae3c33fd27461320
picerusonix <62857510+picerusonix@users.noreply.github.com>
2020-03-30 21:33:20 +0200

    Update gra.h

--- gra.h
+++ gra.h
@@ -2,7 +2,7 @@
 #define gra_h
 #include "SDL.h"
 #include <iostream>
-
+#define SDL_MAIN_HANDLED
 class gra {
 public:
 	gra();
@@ -20,7 +20,7 @@ public:
 
 
 private:
-
+	int licznik;
 	bool dziala;
 	SDL_Window* window;
 	SDL_Renderer* renderer;
